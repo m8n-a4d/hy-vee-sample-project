0c2f734db2a937a5c42dc5967ca8df8f59b37a31
Meganathan S <meganathans@yugasys.com>
2024-03-29 22:25:57 +0530

    added functionality of apis and country bottons api call

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
